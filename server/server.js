dd98e984a45083d6666502918e7897bf55ffbd82
Akshayathangaraj <akshayathangaraj02@gmail.com>
2025-06-13 23:03:43 +0530

    time zone updates

--- server/server.js
+++ server/server.js
@@ -190,8 +190,7 @@ app.get('/api/donations', async (req, res) => {
 });
 
 // Claim Donation Route — protected with JWT middleware
-await axios.put(
-  `https://food-bridge-server.onrender.com/api/donations/${id}/claim`, authenticateToken, async (req, res) => {
+app.put('/api/donations/:id/claim', authenticateToken, async (req, res) => {
   const donationId = req.params.id;
   const userId = req.user.userId;
 
